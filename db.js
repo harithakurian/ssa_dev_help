21d8bae4a06a2cf9f85fe624a977e0fe366743c1
dinh-git <luan@dinh.tk>
2016-09-22 09:14:06 -0400

    added mongo db

--- db.js
+++ db.js
@@ -1,155 +1,43 @@
-'use strict'
+module.exports = function (db) {
 
-var sqlite3 = require('sqlite3').verbose();
-var db = new sqlite3.Database('InstaScam.db');
+    /*
+     * args: user object (or array of user objects)
+     * return: callback(err, isSuccess)
+     */
+    this.insertUser = function (user, callback) {
+        var usersCollection = db.collection('users');
 
-function setDB(otherDB) {
-    db = otherDB;
-}
-
-//initDB(db);
-
-function initDB() {
-
-    var tableSql = `
-    CREATE TABLE IF NOT EXISTS User (
-        UserName TEXT NOT NULL PRIMARY KEY,
-        Password Text,
-        ProfileName TEXT
-    );
-
-    CREATE TABLE IF NOT EXISTS Picture (
-        PictureId INTEGER NOT NULL PRIMARY KEY,
-        UserName TEXT,
-        ActualFileName TEXT,
-        SystemFileName TEXT,
-        Insrt_TS INTEGER DATETIME DEFAULT CURRENT_TIMESTAMP,
-        FOREIGN KEY(UserName) REFERENCES User(UserName)
-    );
-
-    CREATE TABLE IF NOT EXISTS Comment (
-        CommentId INTEGER NOT NULL PRIMARY KEY,
-        PictureId INTEGER,
-        UserName TEXT,
-        Message TEXT,
-        Insrt_TS INTEGER DATETIME DEFAULT CURRENT_TIMESTAMP,
-        FOREIGN KEY(UserName) REFERENCES User(UserName),
-        FOREIGN KEY(PictureId) REFERENCES Picture(PictureId)
-    );
-    `
-
-    db.exec(tableSql, function (err) {
-        if (err) {
-            console.log(err);
-            return;
-        }
-        console.log('all good');
-    });
-
-}
-
-function insertUser(user) {
-    return new Promise(
-        (resolve, reject) => {
-            var stmt = db.prepare("INSERT INTO User (UserName, Password, ProfileName) VALUES (?, ?, ?)");
-            stmt.run(user.userName, user.password, user.profileName, function (err) {
-                if (err) {
-                    reject(err);
-                }
-                else {
-                    console.log("The ID of the last row inserted is: " + this.lastID);
-                    stmt.finalize();
-                    resolve('success');
-                }
-            });
-        });
-}
-
-function getUser(userName) {
-    return new Promise(
-        (resolve, reject) => {
-            var stmt = db.prepare("SELECT * FROM USER WHERE UserName = ?");
-            stmt.all(userName, function (err, rows) {
-                if (err) {
-                    reject(err);
-                }
-                else {
-                    resolve(rows[0]);
-                }
-            });
-        });
-}
-
-function insertPicture(userName, fileName, fileTs) {
-    console.log("db.js insertPicture userName: " + userName)
-    console.log("db.js insertPicture fileName: " + fileName)
-    console.log("db.js insertPicture fileTs: " + fileTs)
-    return new Promise(function (resolve, reject) {
-        var stmt = db.prepare(`INSERT INTO Picture (UserName, ActualFileName, SystemFileName) VALUES (?, ?, ?)`);
-        stmt.run(userName, fileName, fileTs, function (err) {
+        usersCollection.insert(user, function (err, result) {
             if (err) {
-                reject(err);
-                return;
+                callback(err, false);
+            } else {
+                console.log('Inserted documents with:', result);
+                callback(null, true);
             }
-            console.log(this.lastID);
-            resolve(this.lastID);
         });
-        stmt.finalize();
-    });
-}
+    }
 
-function getPictures(userName) {
-    console.log("db.js getPictures(userName): " + userName)
-    return new Promise(function (resolve, reject) {
-        var stmt = "SELECT * FROM Picture where UserName = ?";
-        db.all(stmt, userName, function (err, rows) {
+    /*
+     * arg: filter
+     * return: callback(err, results)
+     */
+    this.findUsers = function (filter, callback) {
+        var collection = db.collection('users');
+        collection.find(filter).toArray(function (err, docs) {
             if (err) {
-                reject(err);
-                return;
+                callback(err, null);
+            } else {
+                callback(null, docs);
             }
-            resolve(rows);
         });
-    });
+    }
+
+    /*
+     * Close database
+     */
+    this.close = function () {
+        db.close();
+    }
+
+    return this;
 }
-
-function insertComment(pictureId, userName, message) {
-    console.log("db.js insertComment pictureId: " + pictureId)
-    console.log("db.js insertComment userName: " + userName)
-    console.log("db.js insertComment message: " + message)
-    return new Promise(function (resolve, reject) {
-        var stmt = db.prepare(`INSERT INTO Comment (PictureId, UserName, Message) VALUES (?, ?, ?)`);
-        stmt.run(pictureId, userName, message, function (err) {
-            if (err) {
-                reject(err);
-                return;
-            }
-            console.log(this.lastID);
-            resolve(this.lastID);
-        });
-        stmt.finalize();
-    });
-}
-
-function getPictureComments(pictureId) {
-    console.log("db.js getPictureComments(pictureId): " + pictureId)
-    return new Promise(function (resolve, reject) {
-        var stmt = "SELECT CommentId as CommentId, PictureId as PictureId, UserName as UserName, Message as Message, Insrt_TS as Date FROM Comment where PictureId = ?";
-        db.all(stmt, pictureId, function (err, rows) {
-            if (err) {
-                reject(err);
-                return;
-            }
-            resolve(rows);
-        });
-    });
-}
-
-exports.insertUser = insertUser;
-exports.getUser = getUser;
-exports.setDB = setDB;
-exports.initDB = initDB;
-exports.insertPicture = insertPicture;
-exports.getPictures = getPictures;
-exports.insertComment = insertComment;
-exports.getPictureComments = getPictureComments;
-
--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 var express = require('express'),
     app = express(),
     bodyParser = require('body-parser'),
-    db = require('./db'),
     path = require('path'),
     mkdirp = require('mkdirp');
+
+var MongoClient = require('mongodb').MongoClient;
+var db;
 
 var multer = require('multer'),
     bodyParser = require('body-parser'),
@@ -47,27 +49,7 @@ app.post('/login/', function (req, res) {
     console.log('login UserName is: ' + login.userName);
     console.log('password is: ' + login.password);
 
-    var p = db.getUser(login.userName);
-    p.then(
-        (val) => {
-
-            if (login.password === val.Password) {
-                req.session.currentUser = {
-                    userName: login.userName
-                };
-
-            } else {
-                throw 'Username or password issue!';
-            }
-            res.redirect('/');
-        }
-    ).catch(
-        (err) => {
-            res.status(500);
-            console.log(err);
-            res.send(err);
-        }
-    )
+    // @TODO
 });
 
 app.post('/insertUser/', function (req, res) {
@@ -77,162 +59,23 @@ app.post('/insertUser/', function (req, res) {
         profileName: req.body.profileName
     };
 
-
-    var p = db.insertUser(user);
-    p.then(
-        (val) => {
-            var dirPath = '/public/photos/' + user.userName;
-            mkdirp(dirPath, function (err) {
-                if (err) {
-                    console.log(err);
-                }
-            });
-            res.send('User ' + user.userName + ' is added successfully!');
-        }
-    ).catch(
-        (err) => {
-            console.log(err);
-            res.send(err);
-        }
-    )
-});
-
-app.post('/insertPicture/', multer({dest: './public/photos/'}).single('dropzone'), function (req, res) {
-
-    //var time = new Date();
-
-    var user = {
-        userName: req.body.userName
-    };
-
-    var file = {
-        actualFileName: req.file.originalname,
-        systemFileName: req.file.filename,
-        filePath: req.file.path
-
-    };
-
-
-    var insertPic = db.insertPicture(user.userName, file.actualFileName, file.systemFileName);
-
-    fs.move('./public/photos/' + req.file.filename, './public/photos/' + user.userName + '/' + file.systemFileName, function (err) {
-        if (err) return console.error(err)
-
-    })
-
-    insertPic.then((val) => {
-        res.send('Picture for ' + user.userName + ' is added successfully!');
-    }).catch(
-        (err) => {
-            console.log(err);
-            res.send(err);
-        }
-    );
-
-});
-
-//app.get('/getPictures/:userName', function (req, res) {
-app.get('/getPictures/', function (req, res) {
-    var picture = {
-        userName: req.session.currentUser,
-    };
-
-
-    var thePictures = db.getPictures(picture.userName);
-
-    thePictures.then(
-        (pictures) => {
-            console.dir(pictures);
-            res.send(pictures);
-        }
-    ).catch(
-        (err) => {
-            res.status(500);
-            res.send('output app.get getPictures error: issue getting Pictures');
-        }
-    );
-})
-
-app.post('/insertComment/', function (req, res) {
-
-    var picture = {
-        pictureId: req.body.pictureId,
-        userName: req.body.userName,
-        message: req.body.comment
-    };
-
-
-    var insertPic = db.insertComment(picture.pictureId, picture.userName, picture.message);
-
-    insertPic.then((val) => {
-        res.send('Comment for ' + picture.pictureId + ' is added successfully!');
-    }).catch(
-        (err) => {
-            console.log(err);
-            res.send(err);
-        }
-    )
-
-});
-
-app.get('/getPictureComments/', function (req, res) {
-
-    var picture = {
-        pictureId: req.body.pictureId,
-    };
-
-
-    var thePictureComments = db.getPictureComments(picture.pictureId);
-
-    thePictureComments.then(
-        (pictureComments) => {
-
-            res.send(pictureComments);
-        }
-    ).catch(
-        (err) => {
-            res.status(500);
-            res.send('output app.get getPictureComments error: issue getting comments');
-        }
-    );
-})
-
-
-app.get('/angular', function (req, res) {
-    res.sendFile(__dirname + '/angularIndex.html');
+    // @TODO
 });
 
 app.get('/', checkAuth, function (req, res) {
     res.sendFile(__dirname + '/index.html');
 });
 
-app.get('/mainApp', function (req, res) {
-    res.send('logged in');
-})
 
-app.get('/adduser', function (req, res) {
+// Initialize connection once
+MongoClient.connect("mongodb://PC93:27017/ssa-dev-help-db", function (err, database) {
+    if (err) {
+        throw err;
+    }
 
-    var user = {
-        userid: req.query.userid,
-        password: req.query.password,
-        firstname: req.query.firstname,
-        lastname: req.query.lastname,
-        email: req.query.email
+    db = require("./db")(database);
 
-    };
-    var p = dbInsertUser(user);
-    p.then(
-        (val) => {
-            res.send('User Added');
-        }
-    ).catch(
-        (err) => {
-            res.send(err);
-        }
-    )
-});
-
-
-app.listen(8080, function () {
-    console.log('Example app listening on port 8080!');
+    // Start the application after the database connection is ready
+    app.listen(8080);
+    console.log("Listening on port 8080");
 });
