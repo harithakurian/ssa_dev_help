28aa827fe469aa57ed4d02702365d229ba9d1e14
ramsahota <ronsahota@hotmail.com>
2016-09-26 09:05:09 -0400

    modified mongodb connection URL to point to PC93

--- server.js
+++ server.js
@@ -263,7 +263,7 @@ app.get('/register', function (req, res) {
 });
 
 // Initialize connection once
-MongoClient.connect("mongodb://localhost:27017/ssa-dev-help-db", function (err, database) {
+MongoClient.connect("mongodb://PC93:27017/ssa-dev-help-db", function (err, database) {
     if (err) {
         throw err;
     }
