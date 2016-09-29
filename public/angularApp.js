23e5bc423fdc3bf13858a9c0b23b383d232166ff
Bill O'Brien <airman.obrien@gmail.com>
2016-09-29 09:18:32 -0400

    i have no idea

--- public/angularApp.js
+++ public/angularApp.js
@@ -136,6 +136,14 @@ app.controller('getQuestionsController', function ($scope, $http, $routeParams, 
         });
     })
 
+    $scope.tinymceOptions ={
+        baseUrl: '/static/js/tinymce',
+        toolbar: 'undo redo | bold italic | alignleft aligncenter alignright | code',
+        plugins: "textcolor colorpicker",
+        skin: 'lightgray',
+        theme : 'modern'
+};
+
     var userName = $routeParams.userName;
     var url = "/getQuestions/";
     if (userName) {
